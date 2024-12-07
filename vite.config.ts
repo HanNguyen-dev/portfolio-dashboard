dda32ed9ddd599b7d7d89765090822b8a58c5b7e
Han Nguyen <han.nguyen@outlook.com>
2024-12-07 00:03:41 -0500

    update vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-    css: true,
-    reporters: ["verbose"],
-    coverage: {
-      reporter: ["text", "json", "html"],
-      include: ["src/**/*"],
-      exclude: [],
+export default defineConfig(() => {
+  return {
+    base: '/',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api1': {
+          target: process.env.INFLATION_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api1/, '/'),
+        },
+        '/api2': {
+          target: process.env.WEATHER_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api2/, '/'),
+        },
+      },
     },
-  },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+      css: true,
+      reporters: ['verbose'],
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+        include: ['src/**/*'],
+        exclude: [],
+      },
+    },
+  };
 });
